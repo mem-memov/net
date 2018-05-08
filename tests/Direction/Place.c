1ef699454873957e1d863eff21416fe67131e7fe
Alexander Novikov <mem-memov@yandex.ru>
2018-05-08 10:05:34 +0300

    direction test

--- tests/Direction/Place.c
+++ tests/Direction/Place.c
@@ -3,12 +3,20 @@
 
 struct Place
 {
-
+	size_t value;
+	size_t position;
+	char * method;
 };
 
 struct Place * Place_mock()
 {
-
+	struct Place * this = malloc(sizeof(struct Place));
+	
+	this->value = 55555;
+	this->position = 55555;
+	this->method = "method not specified";
+	
+	return this;
 }
 
 void Place_destruct(struct Place * this)
@@ -18,17 +26,21 @@ void Place_destruct(struct Place * this)
 
 void Place_bind(struct Place * this, size_t position)
 {
-
+	this->method = "Place_bind";
+	this->position = position;
 }
 
 size_t Place_get(struct Place * this)
 {
-
+	this->method = "Place_get";
+	
+	return this->value;
 }
 
 void Place_set(struct Place * this, size_t value)
 {
-
+	this->method = "Place_set";
+	this->value = value;
 }
 
 char Place_isZero(struct Place * this)
--- tests/Direction/main.c
+++ tests/Direction/main.c
@@ -75,17 +75,6 @@ void it_contructs_an_incoming_direction()
 	demolishTest();
 }
 
-void it_accepts_help_structures_to_avoid_eternal_cycle_when_constructing()
-{
-	prepareTest();
-
-	Direction_setPool(outgoing, nextOutgoing);
-	
-	assert(outgoing->nextDirection == nextOutgoing && "A direction requires a helping set of pointers to operate on the next direction.");
-	
-	demolishTest();
-}
-
 void it_supplies_its_place_inside_the_storage_array()
 {
 	prepareTest();
@@ -102,142 +91,177 @@ void it_supplies_its_place_inside_the_storage_array()
 
 void it_writes_values_of_a_fresh_direction_to_the_store()
 {
-    // 6 -> 12
-	//                 0             6              12              18
-	size_t places[] = {0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  7,7,7,7,7,7};
-	//                                                               ^ outgoing
-	prepareTest(places);
+	// 0             6 node         12 node         18 6->12
+	// 0,0,0,0,0,0,  6,0,0,0,0,0,   12,0,0,0,0,0,   0,0,0,0,0,0
+	// 0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0
+	//                                              ^  ^ ^
+	prepareTest();
 	
 	size_t place = 18;
 	size_t destination = 12;
 	Direction_create(outgoing, place, destination);
 	
-	assert((*outgoing->node) == destination && places[18] == destination && "A direction keeps the node it points to.");
-	assert((*outgoing->previous) == 0 && places[19] == 0 && "A new direction is not connected to any previous direction.");
-	assert((*outgoing->next) == 0 && places[20] == 0 && "A new direction is not connected to any following direction.");
-	
+	assert(
+		0 == strcmp(outgoing->node->method, "Place_set") 
+		&& outgoing->node->value == destination 
+		&& "A direction keeps the node it points to."
+	);
+	assert(
+		0 == strcmp(outgoing->previous->method, "Place_set") 
+		&& outgoing->previous->value == 0 
+		&& "A new direction is not connected to any previous direction."
+	);
+	assert(
+		0 == strcmp(outgoing->next->method, "Place_set") 
+		&& outgoing->next->value == 0 
+		&& "A new direction is not connected to any following direction."
+	);
+
 	demolishTest();
 }
 
 void it_reads_values_of_an_existing_direction_from_the_store()
 {
-    // 6 -> 12
-	//                 0             6              12              18
-	size_t places[] = {0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0};
-	//                                                               ^ outgoing
-	
+	// 0             6 node         12 node         18 6->12
+	// 0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0
+	//                                              ^  ^ ^
 	prepareTest(places);
 	
 	size_t place = 18;
 	Direction_read(outgoing, place);
 	
 	assert(outgoing->place == place && "A direction keeps the place it is bound to inside the storage array.");
-	assert((*outgoing->node) == 12 && "A direction keeps the node it points to.");
-	assert((*outgoing->previous) == 6 && "A direction can be connected to a previous direction.");
-	assert((*outgoing->next) == 0 && "A new direction can be connected to a following direction.");
-	
+	assert(
+		0 == strcmp(outgoing->node->method, "Place_bind") 
+		&& outgoing->node->position == 18
+		&& "A direction keeps the node it points to."
+	);
+	assert(
+		0 == strcmp(outgoing->previous->method, "Place_bind") 
+		&& outgoing->previous->position == 19
+		&& "A direction can be connected to a previous direction."
+	);
+	assert(
+		0 == strcmp(outgoing->next->method, "Place_bind") 
+		&& outgoing->next->position == 20
+		&& "A new direction can be connected to a following direction."
+	);
+
 	demolishTest();
 }
 
 void it_joins_chain_of_directions()
 {
-    // 6 -> 12
-    // 6 -> 18
-	//                 0             6              12              18               24               30
-	size_t places[] = {0,0,0,0,0,0,  6,0,2,0,24,0,  12,0,0,1,0,24,  18,0,0,1,0,30,   12,6,30,6,12,0,  18,0,0,0,0,0};
-	//                               ^ previous                                       ^ next           ^ new
+	// 0             6 node         12 node         18 node          24 6->12         30 6->18
+	// 0,0,0,0,0,0,  6,0,1,0,24,0,  12,0,0,1,0,24,  18,0,0,0,0,0,    12,6,0,6,12,0,   0,0,0,0,0,0
+	// 0,0,0,0,0,0,  6,0,2,0,30,0,  12,0,0,1,0,24,  18,0,0,1,0,30,   12,6,0,6,12,0,   18,6,24,6,18,0
+	//                                                                                   ^  ^
+	prepareTest();
+	
 	size_t previousPlace = 6;
-	
-	prepareTest(places);
-	
-	size_t place = 30;
-	Direction_read(outgoing, place);
-
 	size_t nextPlace = 24;
-	Direction_read(nextOutgoing, nextPlace);
-	
-	Direction_joinChain(outgoing, previousPlace, nextPlace);
 
-	assert((*outgoing->previous) == previousPlace && places[31] == previousPlace && "A direction gets connected to a previous entry (a node).");
-	assert((*outgoing->next) == nextPlace && places[32] == nextPlace && "A new direction gets connected to a following direction.");
+	Direction_joinChain(outgoing, previousPlace, nextPlace);
 	
+	assert(
+		0 == strcmp(outgoingError->method, "DirectionError_forbidZeroAndEqualtyForPreviousAndNext") 
+		&& outgoingError->previous == previousPlace
+		&& outgoingError->next == nextPlace
+	);
+	assert(
+		0 == strcmp(outgoing->previous->method, "Place_set") 
+		&& outgoing->previous->value == previousPlace
+		&& "A direction gets connected to a previous entry (a node)."
+	);
+	assert(
+		0 == strcmp(outgoing->next->method, "Place_set") 
+		&& outgoing->next->value == nextPlace
+		&& "A new direction gets connected to a following direction."
+	);
+
 	demolishTest();
 }
 
 void it_gets_appended_to_a_previous_direction()
 {
-	//                 0             6              12              18
-	size_t places[] = {0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  0,0,0,0,0,0};
-	//                                                              ^ outgoing
-	
-	prepareTest(places);
-	
-	size_t place = 18;
-	Direction_read(outgoing, place);
-	
+	// 0             6 node         12 node         18 6->12
+	// 0,0,0,0,0,0,  6,0,0,0,0,0,   12,0,0,0,0,0,   0,0,0,0,0,0
+	// 0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0
+	//                                                 ^
+	prepareTest();
+
 	size_t previousPlace = 6;
 	Direction_append(outgoing, previousPlace);
 	
-	assert((*outgoing->previous) == previousPlace && places[19] == previousPlace && "A direction gets connected to a previous entry (a node).");
+	assert(
+		0 == strcmp(outgoingError->method, "DirectionError_forbidZeroPlaceForPrevious") 
+		&& outgoingError->previous == previousPlace
+	);
 	
+	assert(
+		0 == strcmp(outgoing->previous->method, "Place_set") 
+		&& outgoing->previous->value == previousPlace
+		&& "A direction gets connected to a previous entry (a node)."
+	);
+
 	demolishTest();
 }
 
 void it_confirms_directing_to_a_particular_node()
 {
-    // 6 -> 12
-	//                 0             6              12              18
-	size_t places[] = {0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0};
-	//                                                               ^ outgoing
-	
-	prepareTest(places);
-	
-	size_t place = 18;
-	Direction_read(outgoing, place);
-	
+	// 0             6 node         12 node         18 6->12
+	// 0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0
+	//                                              ^
+	prepareTest();
+
 	size_t nodePlace = 12;
+	outgoing->value = nodePlace;
 	size_t result = Direction_hasNode(outgoing, nodePlace);
 	
-	assert(result == 1 && "A direction confirms it points to a particular node.");
+	assert(
+		0 == strcmp(outgoing->node->method, "Place_get") 
+		&& result == 1
+		&& "A direction confirms it points to a particular node."
+	);
 	
 	demolishTest();
 }
 
 void it_denies_directing_to_a_particular_node()
 {
-    // 6 -> 12
-	//                 0             6              12              18              24
-	size_t places[] = {0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0,  24,0,0,0,0,0};
-	//                                                               ^ outgoing     ^ some node
-	
-	prepareTest(places);
-	
-	size_t place = 18;
-	Direction_read(outgoing, place);
-	
+	// 0             6 node         12 node         18 6->12        24
+	// 0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0,  24,0,0,0,0,0
+	//                                              ^
+	prepareTest();
+
 	size_t nodePlace = 24;
+	outgoing->node->value = 12;
 	size_t result = Direction_hasNode(outgoing, nodePlace);
 	
-	assert(result == 0 && "A direction denies it points to a particular node.");
-	
+	assert(
+		0 == strcmp(outgoing->node->method, "Place_get") 
+		&& result == 0
+		&& "A direction denies it points to a particular node."
+	);
+
 	demolishTest();
 }
 
 void it_supplies_the_node_it_directs_to()
 {
-    // 6 -> 12
-	//                 0             6              12              18
-	size_t places[] = {0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0};
-	//                                                               ^ outgoing
-	
-	prepareTest(places);
-	
-	size_t place = 18;
-	Direction_read(outgoing, place);
+	// 0             6 node         12 node         18 6->12
+	// 0,0,0,0,0,0,  6,0,1,0,18,0,  12,0,0,1,0,18,  12,6,0,6,12,0
+	//                                              ^
+	prepareTest();
 
+	outgoing->node->value = 12;
 	size_t result = Direction_getNode(outgoing);
 	
-	assert(result == 12 && result == places[18] && "A direction supplies the place a node occupies that it points to.");
+	assert(
+		0 == strcmp(outgoing->node->method, "Place_get") 
+		&& result == 12
+		&& "A direction supplies the place a node occupies that it points to."
+	);
 	
 	demolishTest();
 }
@@ -245,30 +269,28 @@ void it_supplies_the_node_it_directs_to()
 void it_supplies_the_place_of_the_next_direction()
 {
     // 6 -> 12
-    // 6 -> 18
-	size_t places[] = {0,0,0,0,0,0,  6,0,2,0,24,0,  12,0,0,1,0,24,  18,0,0,1,0,30,   12,6,30,6,12,0,  18,24,0,6,18,0};
-	
-	prepareTest(places);
-	
-	size_t place = 24;
-	Direction_read(outgoing, place);
+    // 0             6 node         12 node         18 node          24 6->12        30 6->18
+	// 0,0,0,0,0,0,  6,0,2,0,24,0,  12,0,0,1,0,24,  18,0,0,1,0,30,   12,6,0,6,12,0,  18,6,24,6,18,0
+	//                                                                                    ^
+	prepareTest();
 
 	size_t result = Direction_getNext(outgoing);
 	
-	assert(result == 30 && result == places[26] && "A direction supplies the place of the next direction in the chain.");
-	
+	assert(
+		0 == strcmp(outgoing->next->method, "Place_get") 
+		&& result == 24
+		&& "A direction supplies the place of the next direction in the chain."
+	);
+
 	demolishTest();
 }
 
 void it_gets_deleted_with_reconnection()
 {
-    // 6 -> 12
-    // 6 -> 18 gets deleted
-	//                 0             6              12              18               24               30
-	size_t places[] = {0,0,0,0,0,0,  6,0,2,0,24,0,  12,0,0,1,0,24,  18,0,0,1,0,30,   12,30,0,6,12,0,  18,6,24,6,18,0};
-	//                                                                                                ^ outgoing
-	
-	prepareTest(places);
+	// 0             6 node         12 node         18 node          24 6->12         30 6->18
+	// 0,0,0,0,0,0,  6,0,2,0,24,0,  12,0,0,1,0,24,  18,0,0,1,0,30,   12,30,0,6,12,0,  18,6,24,6,18,0
+	//                                                                                ^
+	prepareTest();
 	
 	Direction_setPool(outgoing, nextOutgoing);
 	
@@ -374,7 +396,6 @@ int main(int argc, char** argv)
 {
 	it_contructs_an_outgoing_direction();
 	it_contructs_an_incoming_direction();
-	it_accepts_help_structures_to_avoid_eternal_cycle_when_constructing();
 	it_supplies_its_place_inside_the_storage_array();
 	it_writes_values_of_a_fresh_direction_to_the_store();
 	it_reads_values_of_an_existing_direction_from_the_store();
